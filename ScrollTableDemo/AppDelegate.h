2936755323b7b10b4cb23b15d1fd683ab67dd9f7
Cloudox <cloudox@foxmail.com>
2015-10-21 09:38:04 +0800

    Initial Commit

---
+++ ScrollTableDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollTableDemo
+//
+//  Created by csdc-iMac on 15/10/21.
+//  Copyright (c) 2015年 csdc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
